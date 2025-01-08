62f2cdb4e526da195a27cee9f413b83330dbef18
flotoine <florian.antoine.m@gmail.com>
2025-01-08 18:31:46 +0100

    table separation

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <script setup>
 import { ref } from 'vue';
+import demoNameListJson from './assets/demo/demoNameList.json';
+
   const nameList = ref([])
   const name = ref("")
   const numberOfTables = ref(0);
   const chairsPerTable = ref(0)
 
   const invalidName = ref(false)
+
   demoNameList()
   function demoNameList () {
-    nameList.value = ["Léo", "Chloé", "Hugo", "Emma", "Gabriel", "Léa", "Lucas", "Camille", "Théo", "Jade", "Maxime", "Clara", "Nathan", "Sarah", "Louis", "Zoé", "Éloïse", "Martin", "Alice", "Arthur", "Manon", "Raphaël", "Louise", "Pierre", "Émilie", "Samuel", "Anaïs", "Adrien", "Margaux", "Thomas", "Inès", "Valentin", "Marion", "Lucien", "Lila", "Augustin", "Solène", "Baptiste", "César", "Léon", "Noémie", "Clara", "Éléonore", "Sacha", "Salomé", "Victor", "Myriam", "Ézéchiel", "Louna", "Léandro", "Mélissa", "Naïma", "Idriss", "Amira"]
+    nameList.value = demoNameListJson
   }
 
   function deleteAll() {
@@ -18,9 +21,9 @@ import { ref } from 'vue';
   function addName () {
     const nameRegex = /^[a-zA-ZÀ-Ÿ-]{2,20}$/;
 
-    if (nameRegex.test(name.value)&&!nameList.value.includes(name._value)) { // checks sets of characters and if name already in list
+    if (nameRegex.test(name.value)&&!nameList.value.some(item => item.name === name.value)) { // checks sets of characters and if name already in list
       invalidName.value = false
-      nameList.value.push(name.value)
+      nameList.value.push({"name": name.value, "table": 0})
     } else {
       invalidName.value = true
     }
@@ -54,9 +57,10 @@ import { ref } from 'vue';
       <button @click="addName">Ajouter</button>
       <p v-if="invalidName">Le nom rentré est invalide ou déjà dans la liste</p>
     </div>
+    <br>
     <div class="list">
       <div class="list_name" @click="deleteName(name)" v-for="name in nameList" :key="name.id">
-        {{ name }}
+        {{ name.name }}
       </div>
     </div>
   </div>
